7583e1319c41506d7420cc18af05964fc1c339ac
Muhammad Amir Yusof Bin Mhd Asri <amiryusof@Amirs-MacBook-Air.local>
2025-04-28 12:55:57 +0800

    ok

--- linkedlist.hpp
+++ linkedlist.hpp
@@ -1,94 +1,26 @@
-#ifndef LINKED_LIST_HPP
-#define LINKED_LIST_HPP
+#ifndef LINKEDLIST_HPP
+#define LINKEDLIST_HPP
 
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <cstring>  // For string operations
-#include <cstdlib>  // For dynamic memory allocation
-#include <chrono>   // For performance measurement
-#include "array.hpp" 
+#include <cstring>
 
-// Transaction node structure for linked list
+// Node Structure for Transaction
 struct NodeTransaction {
     Transaction data;
     NodeTransaction* next;
 };
 
-// Review node structure for linked list
+// Node Structure for Review
 struct NodeReview {
     CustomerReview data;
     NodeReview* next;
 };
 
-// Word frequency node for linked list
+// Node Structure for Word Frequency
 struct NodeWordFreq {
     char word[MAX_STRING_LENGTH];
     int frequency;
     NodeWordFreq* next;
 };
 
-// Linked list class for transactions
-class TransactionList {
-private:
-    NodeTransaction* head;
-    int size;
 
-public:
-    TransactionList();
-    ~TransactionList();
-    
-    // Basic operations
-    void insert(Transaction transaction);
-    void display();
-    int getSize() const;
-    NodeTransaction* getHead() const;
-    void clear();
-    
-    // Core functionality - parallel to array implementation
-    void loadFromCSV(const char* filename);
-    void searchByCustomerID(const char* customerID);
-    void filterTransactions();
-    
-    // Sorting algorithms
-    void bubbleSort();
-    void selectionSort();
-    void insertionSort();
-    void sortByDate();
-    
-    // Performance measurement
-    void measureSortingPerformance();
-};
-
-// Linked list class for reviews
-class ReviewList {
-private:
-    NodeReview* head;
-    int size;
-
-public:
-    ReviewList();
-    ~ReviewList();
-    
-    // Basic operations
-    void insert(CustomerReview review);
-    void display();
-    int getSize() const;
-    NodeReview* getHead() const;
-    void clear();
-    
-    // Core functionality
-    void loadFromCSV(const char* filename);
-    void processReviews();
-};
-
-// Helper functions
-Date parseDate(const char* dateStr);
-int compareDates(const char* date1, const char* date2);
-bool isDateInRange(const char* transactionDate, const char* startDate, const char* endDate);
-void cleanWord(std::string& word);
-
-// Menu function for linked list operations
-void displayLinkedListMenu();
-
-#endif // LINKED_LIST_HPP
+#endif
